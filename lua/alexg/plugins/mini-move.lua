0a7e705c37aadcdf0fc64501e97101c8997bee2e
dc-apg <alex@redcellpartners.com>
2023-10-23 09:33:18 -0500

    Updating Move Commands

--- lua/alexg/plugins/mini-move.lua
+++ lua/alexg/plugins/mini-move.lua
@@ -3,16 +3,16 @@ local minimove_config = function()
         -- Module mappings. Use `''` (empty string) to disable one.
         mappings = {
             -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-            left = '<M-h>',
-            right = '<M-l>',
-            down = '<M-j>',
-            up = '<M-k>',
+            left = '<M-S-h>',
+            right = '<M-S-l>',
+            down = '<M-S-j>',
+            up = '<M-S-k>',
 
             -- Move current line in Normal mode
-            line_left = '<M-h>',
-            line_right = '<M-l>',
-            line_down = '<M-j>',
-            line_up = '<M-k>',
+            line_left = '<M-S-h>',
+            line_right = '<M-S-l>',
+            line_down = '<M-S-j>',
+            line_up = '<M-S-k>',
         },
 
         -- Options which control moving behavior
